db67f1aea524819867687c3650768c51e7a401bc
leo704 <84240124+leo704@users.noreply.github.com>
2025-11-08 21:02:47 -0300

    Quase finalizado
    adicionado menu, sons, e a lógica da inversão de lados na animação do boneco foi resolvida ao simplesmente adicionar imagens refletidas. Quase completo, depois revejo com mais certeza

--- atividade.py
+++ atividade.py
@@ -1,65 +1,57 @@
-# Escolhi o gênero Platformer
-# Fiz diversos comentários ao longo do código para usar de de apoio, pois há coisas novas para mim
+# Escolhi o genero Platformer
+# pgzrun .\atividade.py
 
-# Tela
 WIDTH = 800
 HEIGHT = 600
-
-# Física
 GRAVITY = 1
 JUMP_STRENGTH = -20
 
-# Listas de Jogo
 platforms = []
 enemies = []
+game_state = 'menu'
+sound_on = True
 
 
 class Player:
-    def __init__(self, x, y, idle_frames, walk_frames):
-        # Guarda as listas de animação
-        self.idle_frames = idle_frames
-        self.walk_frames = walk_frames
-
-        # O Actor começa com o primeiro frame de "parado"
-        self.actor = Actor(self.idle_frames[0], (x, y))
-
+    def __init__(self, x, y, idle_frames_r, walk_frames_r, idle_frames_l, walk_frames_l):
+        # Construtor do Player
+        self.idle_frames_r = idle_frames_r
+        self.walk_frames_r = walk_frames_r
+        self.idle_frames_l = idle_frames_l
+        self.walk_frames_l = walk_frames_l
+        self.actor = Actor(self.idle_frames_r[0], (x, y))
         self.vy = 0
         self.on_ground = False
-
-        # --- Novas variáveis para Animação (Req 7 e 8) ---
         self.is_walking = False
-        self.facing_direction = 1  # 1 = direita, -1 = esquerda
-
+        self.facing_direction = 1
         self.animation_timer = 0
         self.current_frame = 0
-        self.actor.flip_x = True
 
     def draw(self):
         self.actor.draw()
 
     def update(self):
-        """
-        O 'update' principal agora delega para
-        funções de movimento e animação.
-        """
         self.update_movement()
         self.update_animation()
 
     def update_movement(self):
-        # Começamos assumindo que o jogador não está andando
         self.is_walking = False
-
-        # Movimento horizontal
         if keyboard.left:
             self.actor.x -= 5
             self.is_walking = True
-            self.facing_direction = -1  # Marcamos que está virado p/ esquerda
+            self.facing_direction = -1
         elif keyboard.right:
             self.actor.x += 5
             self.is_walking = True
-            self.facing_direction = 1  # Marcamos que está virado p/ direita
+            self.facing_direction = 1
 
-        # --- Gravidade e Colisão com Plataforma
+        # Limites da Tela
+        if self.actor.left < 0:
+            self.actor.left = 0
+        elif self.actor.right > WIDTH:
+            self.actor.right = WIDTH
+
+        # Fisica e Colisao
         self.vy += GRAVITY
         self.actor.y += self.vy
         self.on_ground = False
@@ -70,126 +62,188 @@ class Player:
                     self.vy = 0
                     self.on_ground = True
                     break
-
-        # Lógica do Pulo
+                elif self.vy < 0:
+                    self.actor.top = platform.bottom
+                    self.vy = 0
         if (keyboard.space or keyboard.up) and self.on_ground:
             self.vy = JUMP_STRENGTH
             self.on_ground = False
+            if sound_on:
+                sounds.jump.play()
 
     def update_animation(self):
-        # 'self.animation_timer' age como um 'delay'
-        # Aumentamos 1 a cada frame (60x por segundo)
         self.animation_timer += 1
-        # A cada 10 frames, trocamos a imagem
-        # (Ajuste o '10' para animar mais rápido ou mais devagar)
-
         if self.animation_timer % 8 == 0:
-            self.current_frame += 1  # Avança para o próximo frame
-            # Decide qual lista de animação usar
-            if self.is_walking:
-                active_list = self.walk_frames
+            self.current_frame += 1
+            active_list = None
+            if self.facing_direction == 1:
+                if self.is_walking:
+                    active_list = self.walk_frames_r
+                else:
+                    active_list = self.idle_frames_r
             else:
-                active_list = self.idle_frames
-
-            # Reseta o contador de frames se chegar ao fim da lista
-            # current_frame variável 'i' que percorre o vetor de animação, respeitando a ordem
+                if self.is_walking:
+                    active_list = self.walk_frames_l
+                else:
+                    active_list = self.idle_frames_l
             if self.current_frame >= len(active_list):
                 self.current_frame = 0
-
-            # Define a imagem do Actor para o novo frame
             new_image = active_list[self.current_frame]
             self.actor.image = new_image
 
-        # --- Espelhar a Imagem ---
-        # Isso 'vira' o sprite sem precisar de imagens 'left'
-        if self.facing_direction == -1:
-            self.actor.flip_x = True  # Vira horizontalmente
-        else:
-            self.actor.flip_x = False  # Normal
-
 
 class Enemy:
-    def __init__(self, image_file, x, y, patrol_range=80):
-        self.actor = Actor(image_file, (x, y))
-        self.speed = 2  # Velocidade corrigida
+    def __init__(self, x, y, patrol_range, walk_frames_r, walk_frames_l):
+        self.walk_frames_r = walk_frames_r
+        self.walk_frames_l = walk_frames_l
+        self.actor = Actor(self.walk_frames_r[0], (x, y))
+        self.speed = 2
         self.direction = 1
         self.patrol_min_x = x - patrol_range
         self.patrol_max_x = x + patrol_range
+        self.animation_timer = 0
+        self.current_frame = 0
 
     def draw(self):
         self.actor.draw()
 
     def update(self):
-        self.actor.x += self.speed * self.direction
+        self.update_movement()
+        self.update_animation()
 
+    def update_movement(self):
+        self.actor.x += self.speed * self.direction
         if self.actor.right > self.patrol_max_x:
-            self.direction = -1  # Apenas inverte a direção
+            self.direction = -1
         elif self.actor.left < self.patrol_min_x:
             self.direction = 1
+# ***
+
+    def update_animation(self):
+        self.animation_timer += 1
+        if self.animation_timer % 8 == 0:
+            self.current_frame += 1
+            active_list = None
+            if self.direction == 1:
+                active_list = self.walk_frames_r
+            else:
+                active_list = self.walk_frames_l
+            if self.current_frame >= len(active_list):
+                self.current_frame = 0
+            new_image = active_list[self.current_frame]
+            self.actor.image = new_image
 
 
-# -----------------------------------------------------------------
-try:
-    player_idle_list = ['astro_idle_1', 'astro_idle_2']
-    player_walk_list = ['astro_walk_1', 'astro_walk_2']
+player = None
 
-    player = Player(100, 300, player_idle_list, player_walk_list)
 
-    # 1. Construindo o Chão...
-    for i in range(13):
-        x_pos = 32 + (i * 64)
-        y_pos = 568
-        chao_bloco = Actor('chao', (x_pos, y_pos))
-        platforms.append(chao_bloco)  # (Renomeado)
+# Listas de Animacao
+player_idle_r = ['astro_idle_1', 'astro_idle_2']
+player_walk_r = ['astro_walk_1', 'astro_walk_2']
+player_idle_l = ['astro_idle_left_1', 'astro_idle_left_2']
+player_walk_l = ['astro_walk_left_1', 'astro_walk_left_2']
+enemy_walk_r = ['enemy_walk_1', 'enemy_walk_2']
+enemy_walk_l = ['enemy_walk_left_1', 'enemy_walk_left_2']
 
-    # 2. Construindo a Plataforma 1...
-    for i in range(3):
-        x_pos = 132 + (i * 64)
-        y_pos = 400
-        bloco = Actor('plataforma', (x_pos, y_pos))
-        platforms.append(bloco)  # (Renomeado)
+# Cenario
+for i in range(13):
+    platforms.append(Actor('ground', (32 + (i * 64), 568)))
+for i in range(3):
+    platforms.append(Actor('platform', (132 + (i * 64), 400)))
+for i in range(4):
+    platforms.append(Actor('platform', (532 + (i * 64), 250)))
 
-    # 3. Construindo a Plataforma 2...
-    for i in range(4):
-        x_pos = 532 + (i * 64)
-        y_pos = 250
-        bloco = Actor('plataforma', (x_pos, y_pos))
-        platforms.append(bloco)
+# Criando os Botoes do Menu
+start_button = Rect((300, 225), (200, 50))  # (x, y), (largura, altura)
+sound_button = Rect((300, 325), (200, 50))
+quit_button = Rect((300, 425), (200, 50))
 
-    ### 4. Criando Inimigos (Renomeado) ###
-    enemy_1 = Enemy('inimigo', 550, 193)
-    enemies.append(enemy_1)
+music.play('background_music')
 
-    enemy_2 = Enemy('inimigo', 300, 511)
-    enemies.append(enemy_2)
 
-except FileNotFoundError as e:
-    print(f"\n--- ERRO ---")
-    print(f"Não consegui encontrar um arquivo de imagem: {e.filename}")
-    print("Verifique se todas as imagens (chao, plataforma, inimigo, astro_idle_*, astro_walk_*) estão na pasta 'images/'")
-    print("------------\n")
-    quit()
-# -----------------------------------------------------------------
+def start_game():
+    global player, enemies, game_state
+    enemies = []
+    player = Player(100, 300, player_idle_r, player_walk_r,
+                    player_idle_l, player_walk_l)
+
+    enemies.append(Enemy(550, 193, 80, enemy_walk_r, enemy_walk_l))
+    enemies.append(Enemy(300, 511, 80, enemy_walk_r, enemy_walk_l))
+
+    game_state = 'game'
 
 
 def draw():
     screen.clear()
-
-    for platform in platforms:
-        platform.draw()
-
-    for enemy in enemies:
-        enemy.draw()
-
-    player.draw()
+    if game_state == 'menu':
+        draw_menu()
+    elif game_state == 'game':
+        draw_game()
 
 
 def update():
-    player.update()
+    if game_state == 'menu':
+        update_menu()
+    elif game_state == 'game':
+        update_game()
 
+
+def draw_menu():
+    screen.draw.filled_rect(start_button, 'white')
+    screen.draw.filled_rect(sound_button, 'white')
+    screen.draw.filled_rect(quit_button, 'white')
+
+    screen.draw.text("START", center=start_button.center,
+                     color='black', fontsize=28)
+    screen.draw.text("QUIT", center=quit_button.center,
+                     color='black', fontsize=28)
+
+    if sound_on:
+        screen.draw.text("SOUND ON", center=sound_button.center,
+                         color='black', fontsize=28)
+    else:
+        screen.draw.text("SOUND OFF", center=sound_button.center,
+                         color='black', fontsize=28)
+
+
+def draw_game():
+    for platform in platforms:
+        platform.draw()
+    for enemy in enemies:
+        enemy.draw()
+    player.draw()
+
+
+def update_menu():
+    pass
+
+
+def update_game():
+    global game_state
+    player.update()
     for enemy in enemies:
         enemy.update()
         if player.actor.colliderect(enemy.actor):
-            print("Você foi pego!")
-            player.actor.pos = (100, 300)
-            player.vy = 0
+            print("Game Over!")
+            game_state = 'menu'
+
+
+def on_mouse_down(pos):
+    global game_state, sound_on
+    if game_state == 'menu':
+        if start_button.collidepoint(pos):
+            print("Botao START clicado!")
+            start_game()
+
+        elif quit_button.collidepoint(pos):
+            print("Botao QUIT clicado!")
+            quit()
+
+        elif sound_button.collidepoint(pos):
+            sound_on = not sound_on
+            if sound_on:
+                print("Som LIGADO")
+                music.unpause()
+            else:
+                print("Som DESLIGADO")
+                music.pause()
--- exPyhton.py
+++
@@ -1,9 +0,0 @@
-games = []
-
-while True:
-    a = input()
-    if a == '':
-        break
-    else:
-        games.append(a)
-print(games)
